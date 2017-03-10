0c960e5636fa50bed2c12de2065ba32071705dfb
s5max <s5mini.miky@gmail.com>
2017-03-10 15:36:30 -0400

    header

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,42 @@
 <?php
 
-	//Balise doctype head nav
+	//Requete sql from options
 
-?>
+	$select = $bdd->prepare('SELECT * FROM options');
+	$options = $select->fetchAll(PDO::FETCH_ASSSOC); 
+
+?><!DOCTYPE html>
+<html lang='fr'>
+
+	<head>
+	
+		<meta charset="utf-8">
+		
+		<title><?=$title;?></title>
+		
+	</head>
+
+	<body>
+		
+	<nav>
+
+		<header>
+
+			<h1><?=$options['name'];?></h1>
+
+			<address><?=$options['address'];?></address>
+
+			<p><?=$options['tel'];?></p>
+
+		</header>
+
+		<ul>
+			<li><a href="contact.php">Nous contacter</a></li>
+		</ul>
+
+	</nav>
+
+
+	</body>
+
+</html>
